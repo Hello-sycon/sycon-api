ccaaa55f5aac4d41f9a7ba9dd791a5a151d85e46
Mathieu Legros <mathieu.legros@sycon.fr>
2025-10-06 13:47:30 +0200

    SYC-95 : chare Exceptions class in libs

--- libs/SyconApi/src/sycon_api/__init__.py
+++ libs/SyconApi/src/sycon_api/__init__.py
@@ -1,2 +1,6 @@
 from .sycon_api import SyconApi
-__all__ = ["SyconApi"]
+__all__ = ["SyconApi",
+           "SyconApiInvalidParametersException", 
+           "SyconApiMissingParametersException",
+           "SyconApiBadResponseException",
+           "SyconApiServerErrorResponseException"]
